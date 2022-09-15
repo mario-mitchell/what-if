4a92e7a386544df65c265f33dd7ee37b6dec13a2
mario-mitchell <35703472+mario-mitchell@users.noreply.github.com>
2022-09-15 09:38:07 -0700

    Finished up work

--- app.js
+++ app.js
@@ -21,49 +21,71 @@
 
 
 
-/* Days of the Week
+//Days of the Week
 
- let days = ["Monday", "Tuesday", "Wedensday", "Thursday", "Friday", "Saturday", "Sunday"]
-
- const prompt = require('prompt-sync')({sigint: true});
- let numDay = Number (prompt("What day is it today "))
- let days = ["Monday", "Tuesday", "Wedensday", "Thursday", "Friday", "Saturday", "Sunday"]
-
- console.log("Today is " + days[])*/
+ // const prompt = require('prompt-sync')({sigint: true});
 
 
+// let num1 = Number(prompt("pick a number 1 - 7 "));
+ 
+//  if (num1 === 1) {  
+//      console.log("Monday");
+//  } else if (num1 === 2) {
+//      console.log("Tuesday")
+//  } else if (num1 === 3) {
+//      console.log("Wednesday")
+//  } else if (num1 === 4) {
+//      console.log("Thursday")
+//  }else if (num1 === 5) {
+//      console.log("Friday")
+//  }else if (num1 === 6) {
+//      console.log("Saturday")
+//  }else if (num1 === 7) {
+//      console.log("Sunday")
+//  }else {
+//      console.log ("Error");
+//  }
+
+
+ 
 
 
 
-/* Space Boxing
+
+
+ //Space Boxing
 
 
 
-const prompt = require('prompt-sync')({sigint: true});
-let earthWeight = Number(prompt("What is your Earth Weight? "));
-let venusWeight = (earthWeight * 0.78)
-let marsWeight = (earthWeight * 0.39)
-let jupiterWeight = (earthWeight * 2.65)
-let saturnWeight = (earthWeight * 1.17)
-let uranusWeight = (earthWeight * 1.05)
-let neptuneWeight = (earthWeight * 1.23)
-let planet = (prompt("What planet do you want to know your calculated weight for? "));
-if (planet === "venus") {  
-    console.log("Your Earth Weight calculated on this planets gravity is: " + venusWeight);
-} else if (planet === "mars") {
-    console.log("Your Earth Weight calculated on this planets gravity is: " + marsWeight)
-} else if (planet === "jupiter") {
-    console.log("Your Earth Weight calculated on this planets gravity is: " + jupiterWeight)
-} else if (planet === "saturn") {
-    console.log("Your Earth Weight calculated on this planets gravity is: " + saturnWeight)
-}else if (planet === "uranus") {
-    console.log("Your Earth Weight calculated on this planets gravity is: " + uranusWeight)
-}else if (planet === "neptune") {
-    console.log("Your Earth Weight calculated on this planets gravity is: " + neptuneWeight)
-}else {
-    console.log ("not a planet");
-}
-*/
+// const prompt = require('prompt-sync')({sigint: true});
+// let earthWeight = Number(prompt("What is your Earth Weight? "));
+// let venusWeight = (earthWeight * 0.78)
+// let marsWeight = (earthWeight * 0.39)
+// let jupiterWeight = (earthWeight * 2.65)
+// let saturnWeight = (earthWeight * 1.17)
+// let uranusWeight = (earthWeight * 1.05)
+// let neptuneWeight = (earthWeight * 1.23)
+// let planet = (prompt("What planet do you want to know your calculated weight for? "));
+// if (planet === "venus") {  
+//     console.log("Your Earth Weight calculated on this planets gravity is: " + venusWeight);
+// } else if (planet === "mars") {
+//     console.log("Your Earth Weight calculated on this planets gravity is: " + marsWeight)
+// } else if (planet === "jupiter") {
+//     console.log("Your Earth Weight calculated on this planets gravity is: " + jupiterWeight)
+// } else if (planet === "saturn") {
+//     console.log("Your Earth Weight calculated on this planets gravity is: " + saturnWeight)
+// }else if (planet === "uranus") {
+//     console.log("Your Earth Weight calculated on this planets gravity is: " + uranusWeight)
+// }else if (planet === "neptune") {
+//     console.log("Your Earth Weight calculated on this planets gravity is: " + neptuneWeight)
+// }else {
+//     console.log ("not a planet");
+// }
+
+
+
+// FizzBuzz
+
 
 
 const prompt = require('prompt-sync')({sigint: true});
@@ -79,34 +101,39 @@ if(num1%3 == 0){
 
 
 
-/* Group Sizing
+//Group Sizing
 
-const prompt = require('prompt-sync')({sigint: true});
+// const prompt = require('prompt-sync')({sigint: true});
 
-let classSize = Number(prompt("Enter class size: "));
+// let classSize = Number(prompt("Enter class size: "));
 
-//evenly divisible
+// let groupsOf3 = 0;
+// let groupsOf2 = 0;
 
-if (classSize % 3 === 0) {
-    groupsOf3 = classSize/3;
-//groupsOf2 = 0;
-}
-//remainder 1
-if (classSize % 3 === 1) {
-    classSize -= 4;
-    groupsOf2 = 2;
-    groupsOf3 = classSize / 3;
-}
-//remainder 2
-if (classSize % 3 === 2) {
-    classSize -= 2;
-    groupsOf2 = 1;
-    groupsOf3 = classSize / 3;
-}
+// //evenly divisible
 
-console.log("Groups of 3: " + groupsOf3);
-console.log ("Groups of 2: " + groupsOf2);
-*/
+// if (classSize % 3 === 0) {
+//     groupsOf3 = classSize/3;
+// //groupsOf2 = 0;
+// }
+// //remainder 1
+// if (classSize % 3 === 1) {
+//     classSize -= 4;
+//     groupsOf2 = 2;
+//     groupsOf3 = classSize / 3;
+// }
+// //remainder 2
+// if (classSize % 3 === 2) {
+//     classSize -= 2;
+//     groupsOf2 = 1;
+//     groupsOf3 = classSize / 3;
+// }
+
+// console.log("Groups of 3: " + groupsOf3);
+// console.log ("Groups of 2: " + groupsOf2);
+
+
+
 
 
 
